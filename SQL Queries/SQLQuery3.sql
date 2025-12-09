bb93ffe4345b4d4904d2b5c2715bd5710c150c6e
Ryan <149628271+pont7686@users.noreply.github.com>
2025-12-09 14:37:25 -0500

    Update SQLQuery3.sql

--- SQL Queries/SQLQuery3.sql
+++ SQL Queries/SQLQuery3.sql
@@ -12,7 +12,7 @@ FROM Client C
 JOIN Account A ON C.Client_ID = A.Client_ID;
 
 -- View all transactions for a client
-SELECT C.First_Name, C.Last_Name, B.Transaction_Type, B.Amount, B.Transaction_Date
+SELECT C.First_Name, C.Last_Name, T.Transaction_Type, T.Amount, T.Transaction_Date
 FROM Client C
 JOIN Account A ON C.Client_ID = A.Client_ID
 JOIN Transactions T ON A.Account_ID = T.Account_ID
@@ -23,5 +23,6 @@ SELECT L.Loan_ID, C.First_Name, L.Loan_Type, L.Amount, P.Payment_Date, P.Amount 
 FROM Loan L
 JOIN Client C ON L.Client_ID = C.Client_ID
 LEFT JOIN Loan_Payment P ON L.Loan_ID = P.Loan_ID;
+
 
 
